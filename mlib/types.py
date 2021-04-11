a1e98d676c3fda6174c7d04cc40391097dc10ca9
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-04-11 16:26:15 +0200

    Added value_of and get methods to Enum

--- mlib/types.py
+++ mlib/types.py
@@ -6,12 +6,17 @@ class Enum(Enum):
     def by_str(self, value):
         return self.value
     @classmethod
-    def get(cls, value: str):
-        '''Usage: class.value_of(str)'''
+    def value_of(cls, value: str):
+        '''Get Value of Enum. Usage: class.value_of(str)'''
         try:
             return getattr(cls, str(value), None).value
         except: 
             return
+    @classmethod
+    def get(cls, value: str):
+        '''Get Enum type by value or name'''
+        return getattr(cls, str(value), None) or cls(value)
+
     def __lt__(self, other):
         return self.value < other.value
 
