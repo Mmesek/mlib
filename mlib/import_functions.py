4e9fffbc79d81b17201b6f573c806da5d5281413
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-09-09 03:03:53 +0200

    Added import_modules & docstrings

--- mlib/import_functions.py
+++ mlib/import_functions.py
@@ -1,4 +1,5 @@
 def import_from(dirname: str):
+    """Recursively imports modules from specified directory"""
     import importlib, time, pkgutil, sys, os
     t = time.time()
     dirname = [dirname]+[os.path.join(dirname,o) for o in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, o)) and '__' not in o]
@@ -11,6 +12,27 @@ def import_from(dirname: str):
         times.add((package_name, time.time_ns()-_t))
     f = time.time()
     from .logger import log
+    longest = len(max(times, key=lambda x: len(x[0]))[0])
+    times = sorted(times, key=lambda x: x[1], reverse=True)
+    log.log(1, "Times in ns:\n%s", "\n".join("{:-<{longest}}-{}".format(i[0], i[1], longest=longest) for i in times))
+    log.info("%s Loaded in %s", dirname, f-t)
+
+def import_modules(dirname: str):
+    """Imports modules/packages from specified directory"""
+    import importlib, time, pkgutil, sys, os
+    from .logger import log
+    t = time.time()
+    if not os.path.exists(dirname):
+        log.debug("Couldn't find %s. Not importing anything", dirname)
+        return
+    times = set()
+    for _, package_name, _ in pkgutil.iter_modules([dirname]):
+        _t = time.time_ns()
+        full_package_name = '.'.join([dirname.replace('\\','.').replace('/','.'), package_name])
+        if full_package_name not in sys.modules:
+            importlib.import_module(full_package_name)
+        times.add((package_name, time.time_ns()-_t))
+    f = time.time()
     longest = len(max(times, key=lambda x: len(x[0]))[0])
     times = sorted(times, key=lambda x: x[1], reverse=True)
     log.log(1, "Times in ns:\n%s", "\n".join("{:-<{longest}}-{}".format(i[0], i[1], longest=longest) for i in times))
