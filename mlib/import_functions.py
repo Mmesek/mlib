c0a14af58806ed796cdf4514066e86c4d7f7c886
Mmesek <13630781+Mmesek@users.noreply.github.com>
2024-01-16 14:38:59 +0100

    Add try-catch when one module import fails
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- mlib/import_functions.py
+++ mlib/import_functions.py
@@ -3,28 +3,42 @@ def import_from(dirname: str):
     import importlib, time, pkgutil, sys, os
     from platform import python_version_tuple as ver
     from .logger import log
+
     t = time.time()
-    all_dirnames = [dirname]+[os.path.join(dirname,o) for o in os.listdir(dirname) if os.path.isdir(os.path.join(dirname, o)) and '__' not in o]
+    all_dirnames = [dirname] + [
+        os.path.join(dirname, o)
+        for o in os.listdir(dirname)
+        if os.path.isdir(os.path.join(dirname, o)) and "__" not in o
+    ]
     times = set()
-    path = os.getcwd().replace('\\','/')
+    path = os.getcwd().replace("\\", "/")
     for importer, package_name, _ in pkgutil.iter_modules(all_dirnames):
         _t = time.time_ns()
-        full_package_name = '.'.join([importer.path.replace('\\','/').replace(path + "/", "").replace('/','.'), package_name])
+        full_package_name = ".".join(
+            [importer.path.replace("\\", "/").replace(path + "/", "").replace("/", "."), package_name]
+        )
         if full_package_name not in sys.modules:
-            importlib.import_module(full_package_name)
-            log.debug("Imported %s", full_package_name)
-        times.add((package_name, time.time_ns()-_t))
+            try:
+                importlib.import_module(full_package_name)
+                log.debug("Imported %s", full_package_name)
+            except ImportError as ex:
+                log.exception(ex, exc_info=ex)
+        times.add((package_name, time.time_ns() - _t))
     f = time.time()
     if times:
         longest = len(max(times, key=lambda x: len(x[0]))[0])
         times = sorted(times, key=lambda x: x[1], reverse=True)
-        log.log(1, "Times in ns:\n%s", "\n".join("{:-<{longest}}-{}".format(i[0], i[1], longest=longest) for i in times))
-    log.info("%s Loaded in %s", dirname, f-t)
+        log.log(
+            1, "Times in ns:\n%s", "\n".join("{:-<{longest}}-{}".format(i[0], i[1], longest=longest) for i in times)
+        )
+    log.info("%s Loaded in %s", dirname, f - t)
+
 
 def import_modules(dirname: str):
     """Imports modules/packages from specified directory"""
     import importlib, time, pkgutil, sys, os
     from .logger import log
+
     t = time.time()
     if not os.path.exists(dirname):
         log.debug("Couldn't find %s. Not importing anything", dirname)
@@ -32,14 +46,19 @@ def import_modules(dirname: str):
     times = set()
     for _, package_name, _ in pkgutil.iter_modules([dirname]):
         _t = time.time_ns()
-        full_package_name = '.'.join([dirname.replace('\\','.').replace('/','.'), package_name])
+        full_package_name = ".".join([dirname.replace("\\", ".").replace("/", "."), package_name])
         if full_package_name not in sys.modules:
-            importlib.import_module(full_package_name)
-            log.debug("Imported %s", full_package_name)
-        times.add((package_name, time.time_ns()-_t))
+            try:
+                importlib.import_module(full_package_name)
+                log.debug("Imported %s", full_package_name)
+            except ImportError as ex:
+                log.exception(ex, exc_info=ex)
+        times.add((package_name, time.time_ns() - _t))
     f = time.time()
     if times:
         longest = len(max(times, key=lambda x: len(x[0]))[0])
         times = sorted(times, key=lambda x: x[1], reverse=True)
-        log.log(1, "Times in ns:\n%s", "\n".join("{:-<{longest}}-{}".format(i[0], i[1], longest=longest) for i in times))
-    log.info("%s Loaded in %s", dirname, f-t)
+        log.log(
+            1, "Times in ns:\n%s", "\n".join("{:-<{longest}}-{}".format(i[0], i[1], longest=longest) for i in times)
+        )
+    log.info("%s Loaded in %s", dirname, f - t)
